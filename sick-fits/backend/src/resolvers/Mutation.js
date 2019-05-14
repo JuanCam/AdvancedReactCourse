cbf42120f8885575c274b0e126d6582763ab97ac
Juan Gutierrez <gutierrezjuan@google.com>
2019-05-14 17:41:54 -0500

    GraphQL Yoga and Prisma DB setup, build query and modifier resolver to connect the DB with the app and serve an API

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,11 @@
-const mutations = {};
+const Mutation = {
+    async createItem(parent, args, ctx, info) {
+        // TODO: checkl if we are logged in
+        const item = await ctx.db.mutation.createItem({
+            data: {...args}
+        }, info) // Access prisma DB and mutate
+        return item;
+    }
+};
 
-module.exports = mutations;
+module.exports = Mutation;
--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,3 +1,11 @@
-const Query = {};
+const { forwardTo } = require('prisma-binding');
+
+const Query = {
+    // async items(parent, args, ctx, info) {
+    //     const fetchedItems = await ctx.db.query.items();
+    //     return fetchedItems;
+    // }
+    items: forwardTo('db')
+};
 
 module.exports = Query;

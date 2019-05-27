7c8ef2b2697b98a1524fd40f4f08d8e64dce9264
Juan Gutierrez <juangutierrez@Juans-MacBook-Air.local>
2019-05-27 12:15:15 -0500

    Add prod front end endpoint

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -38,7 +38,7 @@ server.express.use(async (req, res, next) => {
 server.start({
     cors: {
         credentials: true,
-        origin: process.env.FRONTEND_URL,
+        origin: [process.env.FRONTEND_URL, 'https://sickfits-reactnext-prod.herokuapp.com'],
     },
 }, deets => {
     console.log(`Server is now running on port ${deets.port}`);
